40d75f9b47fe07a86ddd574bf201aa902fb5fc74
argonaultes <gael@argonaultes.fr>
2025-02-05 18:22:18 +0100

    add 0.n between item and store

--- mylists/models.py
+++ mylists/models.py
@@ -5,9 +5,14 @@ from django.db import models
 class Item(models.Model):
     item_name = models.CharField(max_length=50)
     item_serial = models.IntegerField()
+    store = models.ForeignKey('Store', on_delete=models.CASCADE, null=True)
+
 
     def __str__(self):
         return self.item_name
 
 class Store(models.Model):
     store_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.store_name
