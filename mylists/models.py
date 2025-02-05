1ccc8a577a65c9d17a3efaecefa96a19c4839059
argonaultes <gael@argonaultes.fr>
2025-02-05 16:57:19 +0100

    new models Store and Item

--- mylists/models.py
+++ mylists/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):
+    item_name = models.CharField(max_length=50)
+    item_serial = models.IntegerField()
+
+    def __str__(self):
+        return self.item_name
+
+class Store(models.Model):
+    store_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.store_name
